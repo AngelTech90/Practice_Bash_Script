ae6753b654ccec294f81d3bcf08aee434db92287
Angel Augusto Molina Sayaverde <angelmolinaimas@gmail.com>
2024-11-02 09:49:56 -0400

    Upgrade of a new script

---
+++ Script-2/clone_repos.sh
@@ -0,0 +1,7 @@
+cd ../../;
+
+git clone https://github.com/AngelTech90/Practice_PowerShell;
+git clone https://github.com/AngelTech90/Prove_Gmail_templates;
+
+cd Practice_Bash_Script && git fetch && cd ../Prove_Gmail_templates;
+git fetch;
